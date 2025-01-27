b18182c86178889f49921cf1630a0e76dc01b259
DianKartiko <auliakartiko28@gmail.com>
2025-01-27 18:44:32 +0700

    first Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+  <h1 class="text-3xl font-semibold text-center text-sky-500 py-5">
+    Hello world!
+  </h1>
+    </>
+  )
+}
+
+export default App
